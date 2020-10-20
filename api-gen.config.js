d884c2f17b31663de8ad5fe0dba9ac28b027b2d2
tangdouxiao <1124428668@qq.com>
2020-10-20 10:26:45 +0800

    fix: 333

--- api-gen.config.js
+++ api-gen.config.js
@@ -3,16 +3,17 @@ const path = require("path");
 // 默认配置
 module.exports = {
   output: path.resolve(__dirname, "./apis"),
-  // tpl: path.resolve(__dirname, './swagger/resolved.yml'),
-  //   tpl: [
-  //     {
-  //       path: path.resolve(__dirname, "./swagger/resolved.yml"),
-  //     },
-  //     {
-  //       path: path.resolve(__dirname, "./swagger/resolved_1.yml"),
-  //     },
-  //   ],
-  tpl: {
-    dir: path.resolve(__dirname, "./swagger"),
-  },
+  tpl: path.resolve(__dirname, "./swagger/resolved.yml"),
+  // tpl: [
+  //   {
+  //     path: path.resolve(__dirname, "./swagger/resolved.yml"),
+  //   },
+  //   {
+  //     path: path.resolve(__dirname, "./swagger/resolved_1.yml"),
+  //   },
+  // ],
+  // tpl: {
+  //   dir: path.resolve(__dirname, "./swagger"),
+  //   setFileName: () => {},
+  // },
 };
--- helpers/files.js
+++ helpers/files.js
@@ -1,17 +1,15 @@
-const { promisify } = require('util')
-const { resolve } = require('path')
-const fs = require('fs')
-const yaml = require('js-yaml')
+const { promisify } = require("util");
+const { resolve } = require("path");
+const fs = require("fs");
 
-const reslovePath = (path) => resolve(__dirname, path)
+const reslovePath = (path) => resolve(__dirname, path);
 
-const readFile = promisify(fs.readFile)
+const readFile = promisify(fs.readFile);
 
-const writeFile = promisify(fs.writeFile)
-
+const writeFile = promisify(fs.writeFile);
 
 module.exports = {
-    reslovePath,
-    readFile,
-    writeFile
-}
+  reslovePath,
+  readFile,
+  writeFile,
+};
